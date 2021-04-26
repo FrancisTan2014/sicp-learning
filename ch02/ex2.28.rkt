49c252a818aae6255f2351931926da955489fee6
FrancisTan <499166435@qq.com>
2021-04-26 11:12:52 +0800

    [feat] Finished the exercise 2.28.

---
+++ ch02/ex2.28.rkt
@@ -0,0 +1,11 @@
+#lang racket
+
+(define (fringe l)
+    (cond ((null? l) '())
+          ((pair? l) (append (fringe (car l))
+                             (fringe (cdr l))))
+          (else (list l))))
+
+(define x (list (list 1 2) (list 3 4)))
+(fringe x)
+(fringe (list x x))
